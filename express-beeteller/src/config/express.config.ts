8b337a369629e949e4d43fad4587ad54050a2d63
Joanderson <sirjoandersonwf@gmail.com>
2024-07-25 05:11:05 -0300

    initial commit

---
+++ express-beeteller/src/config/express.config.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import corsOptions from "./cors.config";
+
+import appRoute from "../routes/app.route";
+//import authRoute from "../routes/auth.route";
+//import userRoute from "../routes/user.route";
+//import currencyRoute from "../routes/currency.route";
+
+const app: express.Application = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+//app.use(cors());
+app.use(cors(corsOptions));
+
+app.use(appRoute);
+//app.use(authRoute);
+//app.use(userRoute);
+//app.use(currencyRoute);
+
+export { app };
